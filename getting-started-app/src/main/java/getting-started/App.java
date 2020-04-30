2a7c7b2942a64701d1408ee547fe06a5a1b13b38
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-04-30 16:02:23 +0530

    Getting Started with Maven Quickstart Archetype

---
+++ getting-started-app/src/main/java/getting-started/App.java
@@ -0,0 +1,13 @@
+package getting-started;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
