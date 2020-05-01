9d688bc672d112593b90a2a85c7c7cfcf10890d1
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-05-02 02:36:09 +0530

    Sample Tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,21 @@
 package org.example;
-
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void avg() {
+        App ob = new App();
+        assertEquals(2,ob.avg(1,2,3));
     }
+
 }
